0249aec50b9c29484955d0c138ff29fbf99f2e24
Elaina <userElaina@Gmail.com>
2021-11-18 11:27:28 +0000

    Delete teams_startup_clear.bat

--- teams_startup_clear.bat
+++
@@ -1,2 +0,0 @@
-reg delete HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Run /v TeamsMachineUninstallerProgramData /f
-reg delete HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Run /v TeamsMachineUninstallerLocalAppData /f
