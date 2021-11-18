24afe803902a755773ccb619392e00fe8d4555a0
Elaina <userElaina@Gmail.com>
2021-11-18 11:27:41 +0000

    Delete winsoft_path.bat

--- winsoft_path.bat
+++
@@ -1,1 +0,0 @@
-setx /M path "%path%;C:\All\Winsoft\Binaries-LuaDist-batteries-0.9.8-Windows-x86\bin;C:\All\Winsoft\ffmpeg-2021-09-22-git-447cf53774-full_build\bin;C:\All\Winsoft\GnuWin;C:\All\Winsoft\ImageMagick-7.0.11-Q16-HDRI;C:\All\Winsoft\LLVM\bin;C:\All\Winsoft\myself;C:\All\Winsoft\Pandoc;"
